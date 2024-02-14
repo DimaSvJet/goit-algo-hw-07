53a8af0c00d7fa438143d2c9ae921c9fdbbb9ced
DimaSv <dimasvjet@gmail.com>
2024-02-14 10:32:31 +0200

    goit-algo-hw-07

---
+++ goit-algo-hw-07-1.py
@@ -0,0 +1,96 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+
+def max_node(root):
+    if not root:
+        return root
+
+    while root.right:
+        root = root.right
+    return root.val
+
+def min_node(root):
+    if not root:
+        return root
+
+    while root.left:
+        root = root.left
+    return root.val
+
+def summ_nodes(root):
+    summ_root = 0
+    if not root:
+        return 0
+    summ_root = summ_nodes(root.left) + root.val + summ_nodes(root.right)
+    return summ_root
+
+# Test
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+root = delete(root, 7)
+print(root)
+print('Max_node: ', max_node(root))
+print('Min_node: ', min_node(root))
+print("summ of all nodes: ", summ_nodes(root))
